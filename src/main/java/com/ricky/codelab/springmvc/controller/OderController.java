0a3ebedc88e908b6ca009277e8ea24a4f3d18b42
Ricky Fung <ricky_feng@163.com>
2016-07-26 10:56:44 +0800

    add @RequestBody demo

---
+++ src/main/java/com/ricky/codelab/springmvc/controller/OderController.java
@@ -0,0 +1,10 @@
+package com.ricky.codelab.springmvc.controller;
+
+/**
+ * ${DESCRIPTION}
+ *
+ * @author Ricky Fung
+ * @create 2016-07-26 10:08
+ */
+public class OderController {
+}
---
+++ src/main/java/com/ricky/codelab/springmvc/domain/Oder.java
@@ -0,0 +1,10 @@
+package com.ricky.codelab.springmvc.domain;
+
+/**
+ * ${DESCRIPTION}
+ *
+ * @author Ricky Fung
+ * @create 2016-07-26 10:13
+ */
+public class Oder {
+}
