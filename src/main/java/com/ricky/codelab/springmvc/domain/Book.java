8b244f9a9947e10b408fe17b6237e2c3388e9ca6
Ricky Fung <ricky_feng@163.com>
2016-08-25 20:02:08 +0800

    add Controller

---
+++ src/main/java/com/ricky/codelab/springmvc/domain/Book.java
@@ -0,0 +1,55 @@
+package com.ricky.codelab.springmvc.domain;
+
+/**
+ * ${DESCRIPTION}
+ *
+ * @author Ricky Fung
+ * @create 2016-08-25 11:56
+ */
+public class Book {
+    private long id;
+    private String isbn;
+    private String name;
+    private double price;
+    private String author;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
