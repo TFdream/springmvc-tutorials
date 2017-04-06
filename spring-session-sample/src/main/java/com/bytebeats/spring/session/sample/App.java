b6b591cd4a2800ccb6dce1467a7744f12adfe368
冯兵兵 <bingbingfeng2@yirendai.com>
2017-04-06 20:21:18 +0800

    add spring session demo

---
+++ spring-session-sample/src/main/java/com/bytebeats/spring/session/sample/App.java
@@ -0,0 +1,10 @@
+package com.bytebeats.spring.session.sample;
+
+/**
+ * ${DESCRIPTION}
+ *
+ * @author Ricky Fung
+ * @date 2017-04-06 19:57
+ */
+public class App {
+}
---
+++ spring-session-sample/src/main/java/com/bytebeats/spring/session/sample/model/User.java
@@ -0,0 +1,46 @@
+package com.bytebeats.spring.session.sample.model;
+
+/**
+ * ${DESCRIPTION}
+ *
+ * @author Ricky Fung
+ * @date 2017-04-06 20:16
+ */
+public class User {
+    private Long id;
+    private String name;
+    private String password;
+    private Integer age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
