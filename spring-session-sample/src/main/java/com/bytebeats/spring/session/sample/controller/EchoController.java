42451d035f99d207bce28c9a431cffe9bbb4074e
Ricky <ricky_feng@163.com>
2017-04-09 16:52:26 +0800

    clean code

--- spring-session-sample/src/main/java/com/bytebeats/spring/session/sample/controller/EchoController.java
+++ spring-session-sample/src/main/java/com/bytebeats/spring/session/sample/controller/EchoController.java
@@ -4,6 +4,8 @@ import com.bytebeats.spring.session.sample.model.User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 /**
@@ -16,7 +18,7 @@ import javax.servlet.http.HttpSession;
 public class EchoController {
 
     @RequestMapping(value = "/query", method = RequestMethod.GET)
-    public User query(String name, HttpSession session){
+    public User query(String name, HttpServletRequest request, HttpSession session){
 
         System.out.println(session);
 
